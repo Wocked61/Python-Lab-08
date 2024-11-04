1f14c462feedd909da1eb9d90c1a2ab81684e2eb
Wocked61 <DylanP@csu.fullerton.edu>
2024-11-03 22:57:21 -0800

    Used inheritance to make a student list and advisor list

--- main.py
+++ main.py
@@ -1,2 +1,57 @@
-import Faculty from people
-import Student from people
+from people import Faculty, Student
+
+students = []
+faculty = []
+
+while True:
+    inp1 = input('''
+      *** TUFFY TITAN CONTACT MAIN MENU
+
+     1. Add faculty
+     2. Print faculty
+     3. Add student
+     4. Print student
+     9. Exit the program
+
+Enter menu choice: ''')
+    
+    if (inp1 == '1'):
+        first = input("Enter first name: ")
+        last = input('Enter last name: ')
+        dep = input('Enter department: ')
+        fac = Faculty(firstname=first, lastname=last, department=dep)
+        faculty.append(fac)
+
+    if (inp1 == '2'):
+        print('''
+======================= FACLUTY =======================
+Record  Name                  Department
+======  ====================  =========================''')
+        advisor_count = 0        
+        for x, fac in enumerate(faculty):
+            print(f"{advisor_count:<7} {fac.firstname:<1} {fac.lastname:<14} {fac.department}")
+            advisor_count += 1
+        
+
+    if (inp1 == '3'):
+        first = input("Enter first name: ")
+        last = input('Enter last name: ')
+        clas = input('Enter class year: ')
+        major = input('Enter major: ')  
+        advis = input('Enter faculty advisor: ')
+        student = Student(firstname= first, lastname= last)       
+        student.set_class(classyear=clas)
+        student.set_major(major=major)
+        student.set_advisor(advisor=advis)
+        students.append(student)
+
+    if (inp1 == '4'):
+        print('''
+===================================== STUDENTS ======================================
+Name                  Class      Major                      Advisor
+====================  =========  =========================  =========================''')
+        for x, student in enumerate(students):
+            print(f"{student.firstname:<1} {student.lastname:<15} {student.classyear:<10} {student.major:<26} {str(faculty[int(student.advisor)])}")
+
+    if (inp1 == '9'):
+        break
